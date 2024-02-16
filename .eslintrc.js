f09e371a43024ca3fc728c3de67e96912c741739
mahi778 <89844272+mahi778@users.noreply.github.com>
2024-02-16 13:59:47 -0500

    lint added

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+module.exports = {
+    extends: "airbnb-base",
+    rules: {
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "single"], // or "double"
+    },
+  };
